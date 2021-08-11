196f7879f41494003e295179375d9d37c66b6f27
Ariel Coronel <gcoronel@vippinn.com>
2021-08-11 08:16:10 -0300

    Ended config webpack

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss'
+
+function App() {
+    return (
+        <div className='app'>
+            <h1>React + Webpack + SASS</h1>
+            <p>La mejor web del mundo con React y Webpack</p>
+        </div>
+    )
+}
+
+export default App
